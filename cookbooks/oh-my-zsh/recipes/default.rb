e4c71e513a12d870c270ac675fa8843e3b3fb803
Richard Dowden <rd@richarddowden.com>
2011-11-12 13:11:38 +0000

    Set zsh defaults and ip plugin

--- cookbooks/oh-my-zsh/recipes/default.rb
+++ cookbooks/oh-my-zsh/recipes/default.rb
@@ -10,3 +10,17 @@ script "oh-my-zsh install from github" do
     curl https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh -o - | sh
   EOS
 end
+
+template "#{ENV['HOME']}/.oh-my-zsh/custom/ip.plugins.zsh" do
+  mode 0644
+  owner ENV['USER']
+  group  Etc.getgrgid(Process.gid).name
+  source "ip.plugin.zsh.erb"
+end
+
+template "#{ENV['HOME']}/.zshrc" do
+  mode 0644
+  owner ENV['USER']
+  group  Etc.getgrgid(Process.gid).name
+  source "dot.zshrc.erb"
+end
